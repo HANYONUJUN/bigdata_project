d572a9a3fa7bc868d2c752fdf65f7b62ba24afbc
hanyongjun <gksdydwns34@naver.com>
2023-09-21 16:31:15 +0900

    project 초기 설정

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="main_title">
+   <h1>안녕하세요!</h1>
+  </div>
+
+<div class="map_sample_line"></div>
+  
+<div class="map"> 
+ <div class="map_border"> 
+  <div class="map_body">
+        <p id="location1"></p>
+        <p id="location2"></p>
+        <p id="location3"></p>
+        <p id="location4"></p>
+        <p id="location5"></p>
+        <p id="location6"></p>
+        <p id="location7"></p>
+        <p id="location8"></p>
+        <p id="location9"></p>
+        <p id="location10"></p>
+    </div>
+  </div>
+</div>
+
+<div class="map_writing">
+  <h2>지도를 통해서 각 예술 활동에 <br>
+    대한 위치와 정보를 확인해보세요!</h2>
+</div>
+
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
+<script lang="ts" setup>
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
+
