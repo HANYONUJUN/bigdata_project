f6fab5a67b3cffb87e1cd51ca6b32d46b706898a
hanyongjun <gksdydwns34@naver.com>
2023-11-06 18:42:49 +0900

    json data components형식으로 import

--- src/assets/components/art.vue
+++ src/assets/components/art.vue
@@ -1,42 +1,45 @@
 <template>
- <div id="selected_input">
+ <div class="selected_input">
+  <span id="json_title">예술공간:</span>
+
   <select id="jsonFile" v-model="selectedFile">
       <option v-for="file in jsonFiles" :value="file">{{ file }}</option>
-    </select>
-    <label for="location">지역 검색:</label>
-    <input id="location" v-model="searchQuery" placeholder="지역을 입력하세요" @keyup.enter="searchLocation" />
-   <hr>
- </div>
+  </select>
 
-  <div id="map" ref="map">
+  <span id="location_title">지역 검색:</span> 
+    <input id="location" v-model="searchQuery" placeholder="지역을 입력하세요" @keyup.enter="searchLocation" />
+</div>
+ <hr>
+
+ <div id="map" ref="map">
   <LMap :zoom="zoom" :center="center">
     <LTileLayer :url="url" :attribution="attribution" />
-    <LMarker v-for="(marker, index) in markers" :key="index" :lat-lng="[marker.latitude, marker.longitude]"
-    :style="{
-      animation: 'bounce-in 0.5s',
-      animationDelay: `${index * 0.1}s`
-  }"
-    >
-      <LPopup>{{ marker.name }}</LPopup>
-    </LMarker>
-  </LMap>
-</div>
-<div id="street-view" ref="streetView"></div>
+      <LMarker v-for="(marker, index) in markers" :key="index" :lat-lng="[marker.latitude, marker.longitude]"
+        :style="{
+        animation: 'bounce-in 0.5s',
+        animationDelay: `${index * 0.1}s`
+      }"
+      @click="showBuildingPhoto(marker)">
+
+        <LPopup id="pop-up">
+          {{ marker.name }}<br>
+          {{ marker.tel }}<br>
+          <a href="{{ marker.home }}">{{ marker.home }}</a><br>
+          <img :src="marker.streetViewImageUrl" id="streetview-image"/>
+        </LPopup>
+      </LMarker>
+    </LMap>
+  </div>
+
+  <button type="button" @click="goback" id="back-btn">이전 페이지로</button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import axios from 'axios';
 import { LMap, LTileLayer, LMarker, LPopup } from '@vue-leaflet/vue-leaflet';
 import 'leaflet/dist/leaflet.css';
-
-
-
-interface Marker {
-  name: string;
-  latitude: number;
-  longitude: number;
-}
+import '../scss/art.scss';
+import useArtData from '../data/useData'; 
 
 export default defineComponent({
   components: {
@@ -46,103 +49,13 @@ export default defineComponent({
     LPopup,
   },
 
-  data() {
-    return {
-      zoom: 13,
-      center: [37.5665, 126.9780],
-      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-      attribution: 'Map data &copy; OpenStreetMap contributors',
-      markers: [] as Marker[],
-      searchQuery: '', // searchQuery 속성 추가
-      selectedFile: '',
-      jsonFiles: ['art_galleries.json'],
-    };
-  },
-  watch: {
-    selectedFile(newFile) {
-      // 선택한 파일에 대한 로직 수행
-      console.log('선택한 파일:', newFile);
-      // 선택한 파일을 사용하여 필요한 작업 수행
-    },
-  },
+  setup() {
+    const artData = useArtData();
 
-  mounted() {
-    this.getData();
-  },
+    return{
+      ...artData,
+    }
+  }
 
-  methods: {
-    getData() {
-  const selectedFilePath = `/api_json/${this.selectedFile}`;
-  axios
-    .get(selectedFilePath)
-    .then((response) => {
-      const data = response.data;
-      const markers: Marker[] = [];
-      data.forEach((item: any) => {
-        if (item.latitude !== undefined && item.longitude !== undefined) {
-           markers.push({
-              name: item.name,
-              latitude: item.latitude,
-              longitude: item.longitude,
-            });
-         }
-       });
-        this.markers = markers;
-      })
-        .catch((error) => console.error(error));
-    },
-
-
-    searchLocation() {
-      const geocodingApiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${this.searchQuery}&limit=1&appid=MY_API_KEY`;
-      axios
-        .get(geocodingApiUrl)
-        .then((response) => {
-          if (response.data.length > 0) {
-            const location = response.data[0];
-            this.center = [location.lat, location.lon];
-            this.getData();
-          } else if (response.data.length === null){
-            console.error('검색된 결과가 없습니다.');
-          }
-        })
-        .catch((error) => console.error(error));
-    },
-  },
 });
 </script>
-  
-<style lang="scss">
-@import "../reaction/utils";
-
-$breakpoint-mobile: 768px;
-$breakpoint-desktop: 1024px;
-
-  #map{
-    height: 680px;
-    border: 1px solid black;
-  }
-  @keyframes bounce-in {
-  0% {
-    transform: translateY(100%);
-    opacity: 0;
-  }
-  100% {
-    transform: translateY(0);
-    opacity: 1;
-  }
-}
-
-// 모바일용 스타일
-@include mobile {
-  #map {
-    height: 550px;
-  }
-  #selected_input {
-     display: inline-block;
-     .input{
-        width: 130px;
-     }
-  }
-}
-</style>
